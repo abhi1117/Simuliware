8f7c424b29746a8545cfebcd149668cf68c74dd8
Abhishek Singh <35772358+abhi1117@users.noreply.github.com>
2024-04-15 12:13:56 +0530

    Add files via upload

---
+++ src/components/Footer/FooterTitle.jsx
@@ -0,0 +1,19 @@
+import { Typography } from '@mui/material'
+import React from 'react'
+
+const FooterTitle = ({ text }) => {
+  return (
+    <Typography
+    variant='h6'
+    component='h6'
+    sx={{
+      fontWeight: '700',
+      pb: 1,
+    }}
+    >
+      { text }
+    </Typography>
+  )
+}
+
+export default FooterTitle
