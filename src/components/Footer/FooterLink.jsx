a85145d4bcf92f21a9eb2781e8fbd75cd36749f4
Abhishek Singh <35772358+abhi1117@users.noreply.github.com>
2024-04-26 10:56:08 +0530

    Add files via upload

---
+++ src/components/Footer/FooterLink.jsx
@@ -0,0 +1,25 @@
+import { Link } from '@mui/material'
+import React from 'react'
+
+const FooterLink = ({ text, to }) => {
+  return (
+    <Link
+      to={to}
+      variant="p"
+      component="a"
+      sx={{
+        fontSize: "0.9rem",
+        fontWeight: "400",
+        textDecoration: "none",
+        color: "#FFFFFF",
+        "&:hover": {
+          color: "#FFFFFF",
+        },
+      }}
+    >
+      {text}
+    </Link>
+  );
+}
+
+export default FooterLink
